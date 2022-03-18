a6222bf0d024afff0d66fc28c5ad81fd6487b673
zest19 <zest210793@gmail.com>
2022-03-18 17:24:13 +0700

    install axios

--- server/index.js
+++ server/index.js
@@ -1,7 +1,28 @@
-const express = require('express');
-const app = express;
-const mysql = require('mysql');
-const cors = require('cors');
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
 
-app.use(cors());
+app.use(cors())
+app.use(express.json())
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "shop",
+})
+
+app.get('/products', (req, res) => {
+    db.query("SELECT * FROM product", (err, result) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result)
+        }
+    })
+})
+const port = 3001;
+app.listen(port, () => {
+    console.log(`Server is running oon port ${port}`)
+})
